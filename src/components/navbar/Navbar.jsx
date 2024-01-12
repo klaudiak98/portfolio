dba2ed4accaf6b2f6d69ed568f2efd60574d51f6
Klaudia Kieryk <klaudiakieryk123@gmail.com>
2024-01-12 11:33:46 +0100

    Create Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <span>Klaudia Kieryk</span>
+      <div className='links'>
+        <a href='#'>About</a>
+        <a href='#'>Skills</a>
+        <a href='#'>Project</a>
+        <a href='#'>Experience</a>
+        <a href='#'>Contact</a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
